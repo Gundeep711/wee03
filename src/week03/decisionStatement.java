d9c55f7fa5b44a93dc6d4f337ea159da3d926ca0
754322 <754322@123-PC41.student.Tor.Loc>
2019-05-21 14:47:21 -0400

    week03

---
+++ src/week03/decisionStatement.java
@@ -0,0 +1,40 @@
+package week03;
+
+import java.util.Scanner;
+
+public class decisionStatement {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("How many software you want to purchase : ");
+		int input=sc.nextInt();
+		double discount=0;
+		if(input>=10 && input<=19)
+		{
+			discount=0.20;
+			
+		}
+		else if(input>=20 && input<=49)
+		{
+			discount=0.30;
+			
+		}
+		else if(input>=50 && input<=99)
+		{
+			discount=0.40;
+		}
+		else if(input>=100)
+		{
+			discount=0.50;
+			
+		}
+		
+		double subTotal=99*input;
+		double discountedAmount=subTotal*discount;
+		double finalAmount=subTotal-discountedAmount;
+		System.out.println("SubTotal : $"+subTotal);
+		System.out.println("Discount : %"+discountedAmount);
+		System.out.println("Final Amount : $"+finalAmount);
+	}
+
+}
